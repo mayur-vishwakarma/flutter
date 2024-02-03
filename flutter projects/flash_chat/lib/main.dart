b562d481700a73d64ff45d1601779baa99156ef9
Mayur <mb.mayurvish@gmail.com>
2024-02-03 11:43:29 +0530

    flash chat

--- flutter projects/flash_chat/lib/main.dart
+++ flutter projects/flash_chat/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flash_chat/Screens/welcomeScreen.dart';
 import 'package:flash_chat/Screens/chatScreen.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
