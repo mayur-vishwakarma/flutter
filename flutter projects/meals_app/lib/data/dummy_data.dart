1327608b8f24e8cd032341c37173917e09166ca9
Mayur <mb.mayurvish@gmail.com>
2024-02-06 23:35:43 +0530

    meals app

--- flutter projects/meals_app/lib/data/dummy_data.dart
+++ flutter projects/meals_app/lib/data/dummy_data.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/model/category.dart';
 import 'package:meals_app/model/meal.dart';
 
-// Constants in Dart should be written in lowerCamelcase.
 const availableCategories = [
   Category(
     id: 'c1',
