5e43532f6b0409dd378178a2f9be0f578f5af49a
Mayur <mb.mayurvish@gmail.com>
2024-02-27 18:43:18 +0530

    http

---
+++ flutter projects/photo_app/lib/imagemodel.dart
@@ -0,0 +1,7 @@
+class ImageModel {
+  final int id;
+  final String url;
+  final String title;
+
+  ImageModel(this.id, this.url, this.title);
+}
---
+++ flutter projects/photo_app/lib/main_page.dart
@@ -0,0 +1,67 @@
+import "package:flutter/material.dart";
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:photo_app/imagemodel.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int count = 1;
+  List<ImageModel> images = [];
+
+ Future<void> updateImage() async {
+  count++;
+  var uri = Uri.parse("https://jsonplaceholder.typicode.com/photos/$count");
+  var response = await http.get(uri);
+  if (response.statusCode == 200) {
+    var data = jsonDecode(response.body.toString());
+    setState(() {
+      images.add(
+        ImageModel(data['id'], data['url'], data['title']));
+    });
+  } else {
+    // Handle error here
+    print('Failed to load image');
+  }
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'PhotoApp',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(10),
+        child: ListView.builder(
+            itemCount: images.length,
+            itemBuilder: (context, index) => Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      Image.network(images[index].url),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(images[index].title),
+                    ],
+                  ),
+                )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: updateImage,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
